98b894c30626df0e5ef1397014376b061866f410
Rakha faiz <rakhafaiz22@gmail.com>
2026-09-19 22:15:56 +0700

    Initial commit

---
+++ nilai/app/Models/SiswaModel.php
@@ -0,0 +1,10 @@
+namespace app\Models;
+
+use CodeIgniter\Model;
+
+class SiswaModel extends Model
+{
+    protected $table = 'siswa';
+    protected $primaryKey = 'id_siswa';
+    protected $allowedFields = ['nama', 'nis', 'kelas', 'jurusan'];
+}
